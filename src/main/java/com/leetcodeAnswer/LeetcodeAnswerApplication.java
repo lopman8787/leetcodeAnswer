6cadca23ce207d43935df5f0f6b235f93b3f5498
longpeng <longpeng@10.1.120.215>
2017-04-19 18:32:25 +0800

    initial

---
+++ src/main/java/com/leetcodeAnswer/LeetcodeAnswerApplication.java
@@ -0,0 +1,12 @@
+package com.leetcodeAnswer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeAnswerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeetcodeAnswerApplication.class, args);
+	}
+}
